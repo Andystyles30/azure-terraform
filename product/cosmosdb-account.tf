4726d7cef7b06d539d8c9225dd332f436f13e6a2
AndyStyles30 <andystyles30@gmail.com>
2022-04-12 18:11:43 +0100

    remove cosmo db temporarily

--- product/cosmosdb-account.tf
+++ product/cosmosdb-account.tf
@@ -1,28 +1,28 @@
-# https://www.terraform.io/docs/providers/azurerm/r/cosmosdb_account.html
+# # https://www.terraform.io/docs/providers/azurerm/r/cosmosdb_account.html
 
-resource "azurerm_cosmosdb_account" "db" {
-  name            = "hw${var.resourcecoprefix}-orderstore-cs-${var.environments[terraform.workspace]}"
-  location            = azurerm_resource_group.hackerwaverg.location
-  resource_group_name = azurerm_resource_group.hackerwaverg.name
-  offer_type          = "Standard"
-  kind                = "GlobalDocumentDB"
+# resource "azurerm_cosmosdb_account" "db" {
+#   name            = "hw${var.resourcecoprefix}-orderstore-cs-${var.environments[terraform.workspace]}"
+#   location            = azurerm_resource_group.hackerwaverg.location
+#   resource_group_name = azurerm_resource_group.hackerwaverg.name
+#   offer_type          = "Standard"
+#   kind                = "GlobalDocumentDB"
 
-  enable_automatic_failover = true
-  enable_free_tier = true
+#   enable_automatic_failover = true
+#   enable_free_tier = true
 
-  consistency_policy {
-    consistency_level       = "Session"
-  }
+#   consistency_policy {
+#     consistency_level       = "Session"
+#   }
 
-  geo_location {
-    prefix            = "hw${var.resourcecoprefix}-cosmosdbgeo-${var.environments[terraform.workspace]}"
-    location          = azurerm_resource_group.hackerwaverg.location
-    failover_priority = 0
-  }
+#   geo_location {
+#     prefix            = "hw${var.resourcecoprefix}-cosmosdbgeo-${var.environments[terraform.workspace]}"
+#     location          = azurerm_resource_group.hackerwaverg.location
+#     failover_priority = 0
+#   }
 
-	tags = {
-      environment = terraform.workspace
-      build       = var.build
-      system      = var.product_name
-  }
-}
+# 	tags = {
+#       environment = terraform.workspace
+#       build       = var.build
+#       system      = var.product_name
+#   }
+# }
