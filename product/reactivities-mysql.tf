5a479443a2a627fdb4410b108b549b81346c5352
AndyStyles30 <andystyles30@gmail.com>
2022-04-13 12:49:39 +0100

    add firewall rules and config for mysql

--- product/reactivities-mysql.tf
+++ product/reactivities-mysql.tf
@@ -9,4 +9,25 @@ resource "azurerm_mysql_flexible_server" "reactivities_mysql" {
   backup_retention_days  = 7
   sku_name               = "B_Standard_B1ms"
 }
+ resource "azurerm_mysql_flexible_server_firewall_rule" "reactivities_mysql_firewall_rule" {
+  name                = "officeVpn"
+  resource_group_name = azurerm_resource_group.hackerwaverg.name
+  server_name         = azurerm_mysql_flexible_server.reactivities_mysql.name
+  start_ip_address    = "52.151.65.254"
+  end_ip_address      = "52.151.65.254"
+}
 
+resource "azurerm_mysql_flexible_server_firewall_rule" "reactivities_mysql_firewall_rule_azure_services" {
+  name                = "AzureServices"
+  resource_group_name = azurerm_resource_group.hackerwaverg.name
+  server_name         = azurerm_mysql_flexible_server.reactivities_mysql.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
+
+resource "azurerm_mysql_flexible_server_configuration" "reactivities_mysql_secure_transport" {
+  name                = "require_secure_transport"
+  resource_group_name = azurerm_resource_group.hackerwaverg.name
+  server_name         = azurerm_mysql_flexible_server.reactivities_mysql.name
+  value               = "OFF"
+}
